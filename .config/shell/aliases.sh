09af5409ca33c620cfa1341cd0b6f72580121989
Willy Vaessen <github@willyvaessen.nl>
2025-09-08 15:48:44 +0200

    sync: 2025-09-08T15:48:44+02:00

--- .config/shell/aliases.sh
+++ .config/shell/aliases.sh
@@ -5,6 +5,15 @@ _ALIASES_SHARED_LOADED=1
 # alleen in interactieve shells
 case $- in *i*) : ;; *) return ;; esac
 
+alias dot='git -C ~/dotfiles'
+
+dot-sync() {
+  dot add -A
+  dot commit -m "sync: $(date -Is)" || true     # commit alleen als er wijzigingen zijn
+  dot pull --rebase --autostash
+  dot push
+}
+
 # portable aliassen/funcs
 alias ls='ls --color=auto'
 alias ll='ls -lah --color=auto'
